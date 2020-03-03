5565e2b797e6a6f688a2134898f449bd3b174f90
Jennifer Greenberg <jennifergreenberg35@gmail.com>
2020-03-03 11:47:04 -0500

    adding raw pdf code

--- LearningMyFriend/PowerPoint.py
+++ LearningMyFriend/PowerPoint.py
@@ -8,4 +8,13 @@ class Powerpoint(Drawable):
 		self.___int_width = None
 		self.___int_height = None
 		self.___organizer_organizer = None
+		
+		
+#PDF converter
+#from fpdf import FPDF
+#pdf = FPDF()
+#pdf.add_page()
+#pdf.set_font("Arial", size=12)
+#pdf.cell(200, 10, txt="Welcome to Python!", ln=1, align="C")
+#pdf.output("simple_demo.pdf")
 
