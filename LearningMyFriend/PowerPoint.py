8f215e881724265b3e3038417ac7664f92ebbdb0
Jennifer Greenberg <jennifergreenberg35@gmail.com>
2020-03-03 11:51:07 -0500

    Update PowerPoint.py

--- LearningMyFriend/PowerPoint.py
+++ LearningMyFriend/PowerPoint.py
@@ -19,12 +19,12 @@ class Powerpoint(Drawable):
 #pdf.output("simple_demo.pdf")
 
 
-#from tkinter import *
-#from winsound import *
+#from Tkinter import *
+#import mp3play
 
 #root = Tk() # create tkinter window
 
-#play = lambda: PlaySound('Sound.wav', SND_FILENAME)
+#f = mp3play.load('Sound.mp3'); play = lambda: f.play()
 #button = Button(root, text = 'Play', command = play)
 
 #button.pack()
